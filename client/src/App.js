25953891060a4494bb0cd2bfab75885c57c82c42
Azim Islam <islamazim123@gmail.com>
2020-06-15 19:15:57 +0100

    Created InputForm component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "./components/header/header";
+import InputForm from "./components/inputform/inputform";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header colour="#B22222">To-Do List</Header>
+      <InputForm/>
     </div>
   );
 }
---
+++ client/src/components/inputform/inputform.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import './inputform.css'
+
+
+export default class InputForm extends React.Component {
+    render() {
+        return (
+            <div class="submit">
+                <TextField style={{width: '40%'}} id="standard-basic" label="Add Todo..." />
+                <Button variant="contained" color="secondary">Add</Button>
+            </div>
+        );
+    }
+}
